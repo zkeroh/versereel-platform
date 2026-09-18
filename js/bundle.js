cf3186e90075393212a5047275a0821c7581ded7
Zkero <zeokilla@gmail.com>
2026-09-18 01:34:06 -0500

    Update bundle.js

--- js/bundle.js
+++ js/bundle.js
@@ -37,7 +37,7 @@
       price: 0,
       downloadUrl: "assets/B08.jpg",
       thumbnail: "assets/B08.jpg",
-      pages:  Array.from({ length: 9 }, (_, i) => `assets/B${String(i).padStart(2, '0')}.jpg`),
+      pages:  Array.from({ length: 8 }, (_, i) => `assets/B${String(i+1).padStart(2, '0')}.jpg`),
       views: 10142,
       tags: ["porn comics", "porn comic free", "hentai english", "adult comic", "bleach", "zkero", "orihime", "rukia", "netorare", "milf", "big tits"],
       createdAt: new Date().toISOString()
